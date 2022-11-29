af2403d06b5cd8a1b89c3a2e93fe6786cd5abf97
josh-reynolds <josh.reynolds@frontier.com>
2022-11-28 22:40:03 -0800

    Chapter 7: Going in Circles

---
+++ Chapter_7/circle_maze.rb
@@ -0,0 +1,9 @@
+require 'polar_grid'
+require 'recursive_backtracker'
+
+grid = PolarGrid.new(8)
+RecursiveBacktracker.on(grid)
+
+filename = "circle_maze.png"
+grid.to_png.save(filename)
+puts "saved to #{filename}"

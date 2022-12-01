5ae722973464283df82cae437983c18c02fe200b
josh-reynolds <josh.reynolds@frontier.com>
2022-11-30 17:44:01 -0800

    Chapter 9: Braiding and Weaving Your Mazes - Part I

---
+++ Chapter_9/recursive_backtracker_demo.rb
@@ -0,0 +1,10 @@
+require 'recursive_backtracker'
+require 'grid'
+
+grid = Grid.new(20, 20)
+RecursiveBacktracker.on(grid)
+grid.braid(0.5)
+
+filename = "recursive_backtracker.png"
+grid.to_png(inset: 0.1).save(filename)
+puts "saved to #{filename}"

8cc47b51c98f6cfe87ea2ade84bd5752302b1e21
josh-reynolds <josh.reynolds@frontier.com>
2022-12-07 17:19:02 -0800

    Chapter 14: Bending and Folding Your Mazes - Part IV

--- Chapter_14/cube_demo.rb
+++ Chapter_14/cube_demo.rb
@@ -1,12 +1,12 @@
 require 'cube_grid'
 require 'recursive_backtracker'
 
-grid = CubeGrid.new(10)
+grid = CubeGrid.new(12)
 RecursiveBacktracker.on(grid)
 
 middle = grid[0, grid.rows / 2, grid.columns / 2]
 grid.distances = middle.distances
 
 filename = "cube.png"
-grid.to_png.save(filename)
+grid.to_png(cell_size: 20, inset:0.1).save(filename)
 puts "saved to #{filename}"
